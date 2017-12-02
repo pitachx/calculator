c068959c2b7c4b3583f2c9254ae66545c671ad8d
Bers <pitachx@gmail.com>
2017-12-02 13:00:36 +0200

    add static code analysis

--- src/main/java/org/testig/calculator/Calculator.java
+++ src/main/java/org/testig/calculator/Calculator.java
@@ -1,6 +1,11 @@
 package org.testig.calculator;
 import org.springframework.stereotype.Service;
 
+/**
+* Main Spring Application.
+*/
+
+
 @Service
 public class Calculator {
 	int sum(int a, int b) {
