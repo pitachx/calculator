7cbf06742ab7f3d119c40c74f5576c80304aa33c
Bers <pitachx@gmail.com>
2017-12-03 22:44:54 +0200

    fix cut

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-CALCULATOR_PORT=$(docker-compose port calculator 8080 | cat -d: -f2)
+CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
 test $(curl localhost:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
 
